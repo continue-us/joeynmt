moved everything over because of one big file in last commit. correect loss calculation, test run with LSTM, loss stays at -50k and does not change => correct loss calculation again.

joeynmt/decoders.py
@@ -101,8 +101,8 @@ class RecurrentDecoder(Decoder):
         self.att_vector_layer = nn.Linear(
             hidden_size + encoder.output_size, hidden_size, bias=True)
 
-        self.output_layer = nn.Linear(hidden_size, vocab_size, bias=False)
-        self._output_size = vocab_size
+        self.output_layer = nn.Linear(hidden_size, emb_size, bias=False)
+        self._output_size = emb_size
 
         if attention == "bahdanau":
             self.attention = BahdanauAttention(hidden_size=hidden_size,
@@ -461,7 +461,6 @@ class TransformerDecoder(Decoder):
     A transformer decoder with N masked layers.
     Decoder layers are masked so that an attention head cannot see the future.
     """
-
     def __init__(self,
                  num_layers: int = 4,
                  num_heads: int = 8,
@@ -499,7 +498,9 @@ class TransformerDecoder(Decoder):
         self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
         self.emb_dropout = nn.Dropout(p=emb_dropout)
-        self.output_layer = nn.Linear(hidden_size, vocab_size, bias=False)
+
+        self.output_layer = nn.Linear(hidden_size, hidden_size, bias=False)
+        #self.output_layer = self.layers[-1].feed_forward.pwff_layer[-2]
 
         if freeze:
             freeze_params(self)
@@ -529,6 +530,7 @@ class TransformerDecoder(Decoder):
         """
         assert trg_mask is not None, "trg_mask required for Transformer"
 
+
         x = self.pe(trg_embed)  # add position encoding to word embedding
         x = self.emb_dropout(x)
 
@@ -540,8 +542,12 @@ class TransformerDecoder(Decoder):
                       src_mask=src_mask, trg_mask=trg_mask)
 
         x = self.layer_norm(x)
-        output = self.output_layer(x)
 
+        # input(('OUTPUT DIM BEFORE', x.shape))
+        output = self.output_layer(x)
+        # input(('OUTPUT DIM AFTER', output.shape))
+
+        # output, hidden, att_scores, att_vectors # <--- last three dont matter for transformer
         return output, x, None, None
 
     def __repr__(self):

joeynmt/helpers.py
@@ -16,7 +16,7 @@ import pkg_resources
 
 import torch
 from torch import nn, Tensor
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 
 from torchtext.data import Dataset
 import yaml
@@ -55,8 +55,8 @@ def make_logger(log_dir: str = None, mode: str = "train") -> str:
     :return: joeynmt version number
     """
     logger = logging.getLogger("") # root logger
-    version = pkg_resources.require("joeynmt")[0].version
-
+    #version = pkg_resources.require("joeynmt")[0].version
+    version =1
     # add handlers only once.
     if len(logger.handlers) == 0:
         logger.setLevel(level=logging.DEBUG)
@@ -198,7 +198,7 @@ def bpe_postprocess(string, bpe_type="subword-nmt") -> str:
 def store_attention_plots(attentions: np.array, targets: List[List[str]],
                           sources: List[List[str]],
                           output_prefix: str, indices: List[int],
-                          tb_writer: Optional[SummaryWriter] = None,
+                          tb_writer = None,
                           steps: int = 0) -> None:
     """
     Saves attention plots.

joeynmt/loss.py
@@ -2,10 +2,83 @@
 """
 Module to implement training loss
 """
-
+import math
 import torch
 from torch import nn, Tensor
 from torch.autograd import Variable
+
+from joeynmt.ive import Logcmk, factory_approx_logcmk
+
+class vMF(nn.Module):
+    """ Von Mises Fisher Loss
+    from https://raw.githubusercontent.com/Sachin19/seq2seq-con/master/onmt/ive.py
+    """
+    def __init__(self, embed_dim, pad_index):
+        super().__init__()
+        self.m = embed_dim
+        self.pad_index = pad_index
+        self.warmup = True
+        self.logcmk_fun = factory_approx_logcmk(self.m)
+
+    def increase_precision(self):
+        self.logcmk_fun = Logcmk.apply
+
+    def forward(self, outputs, targets, target_embeddings):
+
+        loss = []
+        cosine_loss = []
+
+        batch_size = outputs.size(0)
+
+        # permute batch and time dimension to iterate over time
+        outputs_timewise = outputs.permute(1,0,2)
+        target_vectors = target_embeddings(targets)
+        targets_timewise = target_vectors.permute(1,0,2)
+
+        # input(f"batch dim: {batch_size}; outputs: {outputs.shape}, targets: {targets.shape}")
+
+        for t, (out_t, trg_t) in enumerate(zip(outputs_timewise, targets_timewise)):
+
+            trg_vec_norm_t = torch.nn.functional.normalize(trg_t, p=2, dim=-1)
+            out_vec_norm_t = torch.nn.functional.normalize(out_t, p=2, dim=-1)
+
+            # print(nll_loss.shape)
+
+            # print("out_t:",out_t.shape,"trg_t:", trg_t.shape)
+
+            # reg2 = l2 *(out_t @ trg_t.T)
+            lambda2 = 0.1
+            # reg2 = out_vec_norm_t * trg_vec_norm_t
+            cos = out_vec_norm_t @ trg_vec_norm_t.T
+
+            # reg1 = l1 * kappa 
+            lambda1 = 0.02
+            kappa = out_t.norm(p=2, dim=-1)
+
+            # vMF LOSS with both regularisations:
+            # nll_loss = -logcmkapprox(self.m, kappa) + lambda2 * reg2 + lambda1 * kappa
+
+            # nll_loss = - Logcmk.apply(self.m,kappa) + torch.log(1+kappa)*(0.2-(out_vec_norm_t*trg_vec_norm_t).sum(dim=-1))
+            # this runs:
+            # nll_loss = - Logcmk.apply(kappa) + kappa * (lambda2-lambda1*(out_vec_norm_t*trg_vec_norm_t).sum(dim=-1))
+            # print("shapes:", Logcmk.apply(kappa).shape, out_vec_norm_t.shape, trg_vec_norm_t.shape,reg2.shape,kappa.shape)
+            # input()
+            nll_loss = - self.logcmk_fun(kappa) - lambda2 * cos + lambda1 * kappa
+            # nll_loss = - Logcmk.apply(kappa) + kappa*(lambda2-lambda1*(out_vec_norm_t*trg_vec_norm_t).sum(dim=-1))
+            # nll_loss = logcmkapprox(self.m, kappa) + torch.log(1+kappa)*(0.2-(out_vec_norm_t*normed_trg).sum(dim=-1))
+
+            # print("full nll:",nll_loss.shape)
+            # print("reg1:",kappa.shape, "reg2", reg2.shape)
+            # input()
+            mask = targets[:,t].ne(self.pad_index)
+            # input(f"mask: {mask.shape}, nll_loss: {nll_loss.shape}, batch_size: {batch_size}")
+
+            loss_t = nll_loss.masked_select(mask).sum()
+
+            loss.append(loss_t)
+
+        full_loss = torch.stack(loss).sum()
+        return full_loss
 
 
 class XentLoss(nn.Module):
@@ -44,7 +117,7 @@ class XentLoss(nn.Module):
         smooth_dist[:, self.pad_index] = 0
         # masking out padding area (sum of probabilities for padding area = 0)
         padding_positions = torch.nonzero(targets.data == self.pad_index,
-            as_tuple=False)
+            as_iuple=False)
         # pylint: disable=len-as-condition
         if len(padding_positions) > 0:
             smooth_dist.index_fill_(0, padding_positions.squeeze(), 0.0)

joeynmt/model.py
@@ -9,7 +9,7 @@ from torch import Tensor
 import torch.nn.functional as F
 
 from joeynmt.initialization import initialize_model
-from joeynmt.embeddings import Embeddings
+from joeynmt.embeddings import Embeddings, PretrainedEmbeddings
 from joeynmt.encoders import Encoder, RecurrentEncoder, TransformerEncoder
 from joeynmt.decoders import Decoder, RecurrentDecoder, TransformerDecoder
 from joeynmt.constants import PAD_TOKEN, EOS_TOKEN, BOS_TOKEN
@@ -26,7 +26,7 @@ class Model(nn.Module):
                  encoder: Encoder,
                  decoder: Decoder,
                  src_embed: Embeddings,
-                 trg_embed: Embeddings,
+                 trg_embed: Embeddings, # TODO ignored -> remove
                  src_vocab: Vocabulary,
                  trg_vocab: Vocabulary) -> None:
         """
@@ -41,16 +41,20 @@ class Model(nn.Module):
         """
         super().__init__()
 
-        self.src_embed = src_embed
-        self.trg_embed = trg_embed
-        self.encoder = encoder
-        self.decoder = decoder
         self.src_vocab = src_vocab
         self.trg_vocab = trg_vocab
+
+        self.src_embed = src_embed 
+        self.trg_embed = self.src_embed
+        self.encoder = encoder
+        self.decoder = decoder
         self.bos_index = self.trg_vocab.stoi[BOS_TOKEN]
         self.pad_index = self.trg_vocab.stoi[PAD_TOKEN]
         self.eos_index = self.trg_vocab.stoi[EOS_TOKEN]
         self._loss_function = None # set by the TrainManager
+
+        # TODO if continue-us
+        #self.trg_embed = PretrainedEmbeddings(self.trg_vocab)
 
     @property
     def loss_function(self):
@@ -76,16 +80,26 @@ class Model(nn.Module):
                              "{`loss`, `encode`, `decode`}.")
 
         return_tuple = (None, None, None, None)
-        if return_type == "loss":
+        if "loss" in return_type:
             assert self.loss_function is not None
 
-            out, _, _, _ = self._encode_decode(**kwargs)
+            if True: # self.loss_function==vMF:
+                # print('USING vMF')
 
-            # compute log probs
-            log_probs = F.log_softmax(out, dim=-1)
+                preds, _, _, _ = self._encode_decode(**kwargs)
+                # input("preds.shape:{}".format(preds.shape))
 
-            # compute batch loss
-            batch_loss = self.loss_function(log_probs, kwargs["trg"])
+                # compute batch loss
+                batch_loss = self.loss_function(preds, kwargs["trg"], self.trg_embed)
+                #print("LOSS ", batch_loss)
+            else:
+                out, _, _, _ = self._encode_decode(**kwargs)
+
+                # compute log probs
+                log_probs = F.log_softmax(out, dim=-1)
+
+                # compute batch loss
+                batch_loss = self.loss_function(log_probs, kwargs["trg"])
 
             # return batch loss
             #     = sum over all elements in batch that are not pad
@@ -212,7 +226,9 @@ def build_model(cfg: dict = None,
     src_padding_idx = src_vocab.stoi[PAD_TOKEN]
     trg_padding_idx = trg_vocab.stoi[PAD_TOKEN]
 
-    src_embed = Embeddings(
+    # TODO if continue-us 
+    src_embed = PretrainedEmbeddings(
+        src_vocab, trg_vocab,
         **cfg["encoder"]["embeddings"], vocab_size=len(src_vocab),
         padding_idx=src_padding_idx)
 
@@ -226,10 +242,12 @@ def build_model(cfg: dict = None,
             raise ConfigurationError(
                 "Embedding cannot be tied since vocabularies differ.")
     else:
-        trg_embed = Embeddings(
-            **cfg["decoder"]["embeddings"], vocab_size=len(trg_vocab),
-            padding_idx=trg_padding_idx)
+        src_embed = PretrainedEmbeddings(
+        src_vocab, trg_vocab,
+        **cfg["encoder"]["embeddings"], vocab_size=len(src_vocab),
+        padding_idx=src_padding_idx)
 
+    
     # build encoder
     enc_dropout = cfg["encoder"].get("dropout", 0.)
     enc_emb_dropout = cfg["encoder"]["embeddings"].get("dropout", enc_dropout)
@@ -263,6 +281,7 @@ def build_model(cfg: dict = None,
                   src_vocab=src_vocab, trg_vocab=trg_vocab)
 
     # tie softmax layer with trg embeddings
+    """
     if cfg.get("tied_softmax", False):
         if trg_embed.lut.weight.shape == \
                 model.decoder.output_layer.weight.shape:
@@ -272,8 +291,9 @@ def build_model(cfg: dict = None,
             raise ConfigurationError(
                 "For tied_softmax, the decoder embedding_dim and decoder "
                 "hidden_size must be the same."
-                "The decoder must be a Transformer.")
-
+                "The decoder must be a Transformer."
+                f"shapes: output_layer.weight: {model.decoder.output_layer.weight.shape}; target_embed.lut.weight:{trg_embed.lut.weight.shape}")
+    """
     # custom initialization of model parameters
     initialize_model(model, cfg, src_padding_idx, trg_padding_idx)
 

joeynmt/search.py
@@ -103,7 +103,8 @@ def recurrent_greedy(
 # pylint: disable=unused-argument
 def transformer_greedy(
         src_mask: Tensor, max_output_length: int, model: Model,
-        encoder_output: Tensor, encoder_hidden: Tensor) -> (np.array, None):
+        encoder_output: Tensor, encoder_hidden: Tensor,
+        ) -> (np.array, None):
     """
     Special greedy function for transformer, since it works differently.
     The transformer remembers all previous states and attends to them.

joeynmt/training.py
@@ -17,7 +17,7 @@ import numpy as np
 
 import torch
 from torch import Tensor
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 
 from torchtext.data import Dataset
 
@@ -28,7 +28,7 @@ from joeynmt.helpers import log_data_info, load_config, log_cfg, \
     make_logger, set_seed, symlink_update, ConfigurationError
 from joeynmt.model import Model, _DataParallel
 from joeynmt.prediction import validate_on_data
-from joeynmt.loss import XentLoss
+from joeynmt.loss import XentLoss, vMF
 from joeynmt.data import load_data, make_data_iter
 from joeynmt.builders import build_optimizer, build_scheduler, \
     build_gradient_clipper
@@ -68,8 +68,8 @@ class TrainManager:
 
         self.logging_freq = train_config.get("logging_freq", 100)
         self.valid_report_file = "{}/validations.txt".format(self.model_dir)
-        self.tb_writer = SummaryWriter(
-            log_dir=self.model_dir + "/tensorboard/")
+        # self.tb_writer = SummaryWriter(
+        #     log_dir=self.model_dir + "/tensorboard/")
 
         # model
         self.model = model
@@ -77,8 +77,12 @@ class TrainManager:
 
         # objective
         self.label_smoothing = train_config.get("label_smoothing", 0.0)
-        self.model.loss_function = XentLoss(pad_index=self.model.pad_index,
-                                            smoothing=self.label_smoothing)
+        # self.model.loss_function = XentLoss(pad_index=self.model.pad_index,
+        #                                     smoothing=self.label_smoothing)
+        self.model.loss_function = vMF(
+            pad_index=self.model.pad_index,
+            embed_dim=self.model.trg_embed.embedding_dim
+        )
         self.normalization = train_config.get("normalization", "batch")
         if self.normalization not in ["batch", "tokens", "none"]:
             raise ConfigurationError("Invalid normalization option."
@@ -151,6 +155,7 @@ class TrainManager:
         self.shuffle = train_config.get("shuffle", True)
         self.epochs = train_config["epochs"]
         self.batch_size = train_config["batch_size"]
+        # input(f"batch_size: {self.batch_size}")
         # per-device batch_size = self.batch_size // self.n_gpu
         self.batch_type = train_config.get("batch_type", "sentence")
         self.eval_batch_size = train_config.get("eval_batch_size",
@@ -178,7 +183,7 @@ class TrainManager:
                 raise ImportError(
                     "Please install apex from "
                     "https://www.github.com/nvidia/apex "
-                    "to use fp16 training.") from no_apex
+                    "to use fp16 training.") # from no_apex
             self.model, self.optimizer = amp.initialize(
                 self.model, self.optimizer, opt_level='O1')
             # opt level: one of {"O0", "O1", "O2", "O3"}
@@ -375,9 +380,19 @@ class TrainManager:
             batch_loss = 0
 
             for i, batch in enumerate(iter(train_iter)):
+                # try:
+                #     input(self.model.trg_vocab.arrays_to_sentences(
+                #         batch.trg[0].cpu().numpy()
+                #     ))
+                # except TypeError as RE:
+                #     print(RE)
+                #     print([(t.shape, t) for t in batch.trg])
+                #     exit()
+
                 # create a Batch object from torchtext batch
                 batch = self.batch_class(batch, self.model.pad_index,
-                                         use_cuda=self.use_cuda)
+                    use_cuda=self.use_cuda)
+                # input(f"newly created batch: {batch.trg.shape}")
 
                 # get batch loss
                 batch_loss += self._train_step(batch)
@@ -408,8 +423,8 @@ class TrainManager:
 
                     # log learning progress
                     if self.stats.steps % self.logging_freq == 0:
-                        self.tb_writer.add_scalar("train/train_batch_loss",
-                                                  batch_loss, self.stats.steps)
+                        # self.tb_writer.add_scalar("train/train_batch_loss",
+                        #                           batch_loss, self.stats.steps)
                         elapsed = time.time() - start - total_valid_duration
                         elapsed_tokens = self.stats.total_tokens - start_tokens
                         logger.info(
@@ -447,7 +462,7 @@ class TrainManager:
                     self.stats.best_ckpt_iter, self.stats.best_ckpt_score,
                     self.early_stopping_metric)
 
-        self.tb_writer.close()  # close Tensorboard writer
+        # self.tb_writer.close()  # close Tensorboard writer
 
     def _train_step(self, batch: Batch) -> Tensor:
         """
@@ -521,12 +536,12 @@ class TrainManager:
                 n_gpu=self.n_gpu
             )
 
-        self.tb_writer.add_scalar(
-            "valid/valid_loss", valid_loss, self.stats.steps)
-        self.tb_writer.add_scalar(
-            "valid/valid_score", valid_score, self.stats.steps)
-        self.tb_writer.add_scalar(
-            "valid/valid_ppl", valid_ppl, self.stats.steps)
+        # self.tb_writer.add_scalar(
+        #     "valid/valid_loss", valid_loss, self.stats.steps)
+        # self.tb_writer.add_scalar(
+        #     "valid/valid_score", valid_score, self.stats.steps)
+        # self.tb_writer.add_scalar(
+        #     "valid/valid_ppl", valid_ppl, self.stats.steps)
 
         if self.early_stopping_metric == "loss":
             ckpt_score = valid_loss
@@ -584,7 +599,7 @@ class TrainManager:
                 indices=self.log_valid_sents,
                 output_prefix="{}/att.{}".format(
                     self.model_dir, self.stats.steps),
-                tb_writer=self.tb_writer, steps=self.stats.steps)
+                steps=self.stats.steps)
 
         return valid_duration
 